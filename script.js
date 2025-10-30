2c5a725279c093b7ae5fbd3dceb6c99804fb6935
Owen <stefanescuowen@gmail.com>
2025-10-30 17:06:19 -0400

    Forgot to give signup.html a favicon. Added log in and log out functionalities. Added animations for incorrect inputs and also fade-ins. Added a prevention for people to login in without an account to be forced onto the log in page so that they don't go into the website without an account.

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+// Block direct acsess to welcome page
+if (window.location.pathname.includes('welcome.html')){
+    
+    const savedUser = localStorage.getItem('demo_username');
+    const savedPass = localStorage.getItem('demo_password');
+    const isLoggedIn = localStorage.getItem('isLoggedIn');
+    
+    if(!savedUser || !savedPass || isLoggedIn !== 'true'){
+        window.location.href = 'index.html';
+    }
+}
+
+
+// Block direct acsess to login page
+if (window.location.pathname.includes('index.html')){
+   const isLoggedIn = localStorage.getItem('isLoggedIn');
+   if (isLoggedIn === 'true'){
+    window.location.href = 'welcome.html';
+   }
+}
+
+// Dispay username from localStorage
+const userDisplay = document.getElementById('userDisplay');
+const savedUser = localStorage.getItem('demo_username');
+if(userDisplay && savedUser){
+    userDisplay.textContent = savedUser;
+}
+
+
 // Signup Handler
 const signupForm = document.getElementById('signupForm');
 if (signupForm){
@@ -10,13 +39,19 @@ if (signupForm){
         const msg = document.getElementById('signupMessage');
 
         msg.textContent = '';
+        msg.classList.remove('success', 'error');
 
         if (!username || !password){
             msg.textContent = 'Please fill in all fields.';
+            msg.classList.add('error', 'shake');
+            setTimeout(() => msg.classList.remove('shake'), 400);
             return;
         }
+
         if (password !== confirm){
             msg.textContent = 'Passwords do not match.'
+            msg.classList.add('error', 'shake');
+            setTimeout(() => msg.classList.remove('shake'), 400);
             return;
         }
 
@@ -24,6 +59,7 @@ if (signupForm){
         localStorage.setItem('demo_password', password);
 
         msg.textContent = 'Account Created - You may now log in.'
+        msg.classList.add('success');
         signupForm.reset();
     });
 };
@@ -39,21 +75,51 @@ if(loginForm){
         const msg = document.getElementById('loginMessage');
 
         msg.textContent = '';
+        msg.classList.remove('sucess', 'error');
 
         const savedUser = localStorage.getItem('demo_username');
         const savedPass = localStorage.getItem('demo_password');
 
         if (!username || !password){
             msg.textContent = 'Please fill in both fields.';
+            msg.classList.add('error', 'shake');
+            setTimeout(() => msg.classList.remove('shake'), 400);
             return;
         }
 
         if(username === savedUser && password === savedPass){
-            window.location.href = 'welcome.html'
+            msg.textContent = 'Login sucessful!';
+            msg.classList.add('sucess');
+            localStorage.setItem('isLoggedIn', 'true');
+            setTimeout(() =>{
+                window.location.href = 'welcome.html';
+            }, 1000);
+        } else{
+            msg.textContent = 'Incorrect username or password.'
+            msg.classList.add('error', 'shake');
+            setTimeout(() => msg.classList.remove('shake'), 400);
         }
+    });
+}
 
-        else{
-            msg.textContent = 'Incorrect username or password.';
-        }
+// Logout Handler
+const logoutBtn = document.getElementById('logoutBtn');
+if (logoutBtn){
+    logoutBtn.addEventListener('click', () => {
+
+        const logoutMsg = document.createElement('p');
+        logoutMsg.textContent = "Logging out... Catch you later, Phantom Theif!";
+        logoutMsg.classList.add('success', 'logout-message');
+        document.body.appendChild(logoutMsg);
+
+        logoutMsg.style.opacity = '0';
+        logoutMsg.style.transition = 'opacity 0.8s ease';
+        setTimeout(() => logoutMsg.style.opacity = '1', 100);
+        setTimeout(() => logoutMsg.style.opacity = '0', 1500);
+
+        setTimeout(() => {
+            localStorage.removeItem('isLoggedIn');
+            window.location.href = 'index.html';
+        }, 1800);
     });
 }
